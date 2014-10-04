94731b2168c04c6f2029a641495b78e0b212d0a5
Takeo Fujita <takeofujita@gmail.com>
2014-10-04 18:06:49 +0900

    first commit

---
+++ lib/capistrano/opsworks_deploy.rb
@@ -0,0 +1,7 @@
+require "capistrano/opsworks_deploy/version"
+
+module Capistrano
+  module OpsworksDeploy
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/opsworks_deploy/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module OpsworksDeploy
+    VERSION = "0.0.1"
+  end
+end
